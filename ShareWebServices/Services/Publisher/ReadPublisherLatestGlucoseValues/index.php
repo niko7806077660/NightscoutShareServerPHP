29b3d8cc5cd3c99dad29f1053ce1bc0642086797
reelman <jakub@peter.pl>
2019-04-19 19:32:24 +0200

    Update index.php

--- ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues/index.php
+++ ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues/index.php
@@ -1,4 +1,11 @@
 <?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
 // $_ENV['NIGHTSCOUT_SERVER_NAME'] = 'yournightscout.com';
 function directionToInteger($direction) {
   switch($direction) {
