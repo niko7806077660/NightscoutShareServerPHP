2a1bbf30c8fc3f78df119822755c0b1922c6f60a
reelman <jakub@peter.pl>
2019-04-19 19:31:38 +0200

    Update index.php

--- ShareWebServices/Services/General/LoginPublisherAccountByName/index.php
+++ ShareWebServices/Services/General/LoginPublisherAccountByName/index.php
@@ -1,4 +1,11 @@
 <?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
 echo json_encode(
   sprintf(
     '%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
